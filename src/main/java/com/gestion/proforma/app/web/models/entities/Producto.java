13cce8990a155c67a3c42c973fc1e562190d9e60
smperez1997 <smperez7@espe.edu.ec>
2019-11-28 23:00:18 -0500

    CORRECCIÓN DE VALIDACIONES

--- src/main/java/com/gestion/proforma/app/web/models/entities/Producto.java
+++ src/main/java/com/gestion/proforma/app/web/models/entities/Producto.java
@@ -14,8 +14,13 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
 
  
 @Entity
@@ -45,9 +50,12 @@ public class Producto implements Serializable {
 	@Column(name = "IMAGEN")
 	@NotEmpty
 	private String imagen;
-	
+	 
+	@NotNull 
+    @Min(value = 0) 
+    @Max(value = 1000000000) 
 	@Column(name = "PRECIO")
-	@NotEmpty
+     
 	private Float precio;
 
 	@Size(max = 200)
